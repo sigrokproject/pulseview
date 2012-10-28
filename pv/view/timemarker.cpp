cd6c8ee24fe845fc7d4fcbf8672bb8cdd3afea1a
Joel Holdsworth <joel@airwebreathe.org.uk>
2012-10-28 16:22:37 +0000

    Added TimeMarker base class

---
+++ pv/view/timemarker.cpp
@@ -0,0 +1,56 @@
+/*
+ * This file is part of the PulseView project.
+ *
+ * Copyright (C) 2012 Joel Holdsworth <joel@airwebreathe.org.uk>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+#include "timemarker.h"
+
+#include "view.h"
+
+#include <QPainter>
+
+namespace pv {
+namespace view {
+
+TimeMarker::TimeMarker(const View &view, const QColor &colour,
+	double time) :
+	_view(view),
+	_colour(colour),
+	_time(time)
+{
+}
+
+double TimeMarker::time() const
+{
+	return _time;
+}
+
+void TimeMarker::set_time(double time)
+{
+	_time = time;
+}
+
+void TimeMarker::paint(QPainter &p, const QRect &rect)
+{
+	const float x = (_time - _view.offset()) / _view.scale();
+	p.setPen(_colour);
+	p.drawLine(QPointF(x, rect.top()), QPointF(x, rect.bottom()));
+}
+
+} // namespace view
+} // namespace pv
---
+++ pv/view/timemarker.h
@@ -0,0 +1,80 @@
+/*
+ * This file is part of the PulseView project.
+ *
+ * Copyright (C) 2012 Joel Holdsworth <joel@airwebreathe.org.uk>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+#ifndef PULSEVIEW_PV_VIEW_MARKER_H
+#define PULSEVIEW_PV_VIEW_MARKER_H
+
+#include <QColor>
+
+class QPainter;
+class QRect;
+
+namespace pv {
+namespace view {
+
+class View;
+
+class TimeMarker
+{
+protected:
+	/**
+	 * Constructor.
+	 * @param view A reference to the view that owns this marker.
+	 * @param colour A reference to the colour of this cursor.
+	 * @param time The time to set the flag to.
+	 */
+	TimeMarker(const View &view, const QColor &colour, double time);
+
+public:
+	/**
+	 * Gets the time of the marker.
+	 */
+	double time() const;
+
+	/**
+	 * Sets the time of the marker.
+	 */
+	void set_time(double time);
+
+	/**
+	 * Paints the marker to the viewport.
+	 * @param p The painter to draw with.
+	 * @param rect The rectangle of the viewport client area.
+	 */
+	virtual void paint(QPainter &p, const QRect &rect);
+
+	/**
+	 * Paints the marker's label to the ruler.
+	 * @param p The painter to draw with.
+	 * @param rect The rectangle of the ruler client area.
+	 */
+	virtual void paint_label(QPainter &p, const QRect &rect) = 0;
+
+protected:
+	const View &_view;
+	const QColor &_colour;
+
+	double _time;
+};
+
+} // namespace view
+} // namespace pv
+
+#endif // PULSEVIEW_PV_VIEW_MARKER_H
