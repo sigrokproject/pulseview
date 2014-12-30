208c6fc371951fe74ba5a08a0950f2837eef8b7b
Joel Holdsworth <joel@airwebreathe.org.uk>
2014-12-30 14:22:30 +0000

    Viewport: Mouse wheel scrolls vertically when the control key is pressed
    This fixes #497

--- pv/view/viewport.cpp
+++ pv/view/viewport.cpp
@@ -194,18 +194,27 @@ void Viewport::mouseDoubleClickEvent(QMouseEvent *event)
 		view_.zoom(-2.0, event->x());
 }
 
-void Viewport::wheelEvent(QWheelEvent *event)
+void Viewport::wheelEvent(QWheelEvent *e)
 {
-	assert(event);
+	assert(e);
 
-	if (event->orientation() == Qt::Vertical) {
-		// Vertical scrolling is interpreted as zooming in/out
-		view_.zoom(event->delta() / 120, event->x());
-	} else if (event->orientation() == Qt::Horizontal) {
+	if (e->orientation() == Qt::Vertical)
+	{
+		if (e->modifiers() & Qt::ControlModifier) {
+			// Vertical scrolling with the control key pressed
+			// is intrepretted as vertical scrolling
+			view_.set_v_offset(-view_.owner_visual_v_offset() -
+				(e->delta() * height()) / (8 * 120));
+		} else {
+			// Vertical scrolling is interpreted as zooming in/out
+			view_.zoom(e->delta() / 120, e->x());
+		}
+	}
+	else if (e->orientation() == Qt::Horizontal)
+	{
 		// Horizontal scrolling is interpreted as moving left/right
 		view_.set_scale_offset(view_.scale(),
-				       event->delta() * view_.scale()
-				       + view_.offset());
+			e->delta() * view_.scale() + view_.offset());
 	}
 }
 
