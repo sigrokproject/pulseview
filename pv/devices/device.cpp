05b2276928535ae1f8e16b6f9c89802d8d261fee
Jonathan Dahan <jonathan@jonathan.is>
2015-08-10 14:37:34 -0400

    SAMPLERATE is a guint64
    This fixes bug #621.

--- pv/devices/device.cpp
+++ pv/devices/device.cpp
@@ -76,7 +76,7 @@ T Device::read_config(const ConfigKey *key, const T default_value)
 		(*iter).second.find(sigrok::GET) == (*iter).second.end())
 		return default_value;
 
-	return VariantBase::cast_dynamic<Variant<T>>(
+	return VariantBase::cast_dynamic<Glib::Variant<guint64>>(
 		device_->config_get(ConfigKey::SAMPLERATE)).get();
 }
 
