34ec096859764f71c210ec7c271fcac2b96374cc
Joel Holdsworth <joel@airwebreathe.org.uk>
2015-10-03 16:37:23 -0600

    Viewport: Anti-alias time items, but not row items

--- pv/view/viewport.cpp
+++ pv/view/viewport.cpp
@@ -179,10 +179,10 @@ void Viewport::paintEvent(QPaintEvent*)
 	for (const shared_ptr<RowItem> r : row_items)
 		r->paint_mid(p, pp);
 
-	p.setRenderHint(QPainter::Antialiasing, false);
-
 	for (const shared_ptr<RowItem> r : row_items)
 		r->paint_fore(p, pp);
+
+	p.setRenderHint(QPainter::Antialiasing, false);
 	for (const shared_ptr<TimeItem> t : time_items)
 		t->paint_fore(p, pp);
 
