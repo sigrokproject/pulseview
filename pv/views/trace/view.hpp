4eba9990f617cba59193c7df8e6359ea642ccb27
jaseg <git@jaseg.net>
2019-06-08 11:19:48 +0900

    Trace view: Allow setting cursors via shift-drag

--- pv/views/trace/view.hpp
+++ pv/views/trace/view.hpp
@@ -278,6 +278,11 @@ public:
 	 */
 	void show_cursors(bool show = true);
 
+    /**
+     * Sets the cursors to the given offsets. You will still have to call show_cursors separately.
+     */
+    void set_cursors(pv::util::Timestamp& first, pv::util::Timestamp& second);
+
 	/**
 	 * Moves the cursors to a convenient position in the view.
 	 */
