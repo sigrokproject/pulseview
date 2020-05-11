39debd2f895c23ff5da216781d111932b925629e
Uwe Hermann <uwe@hermann-uwe.de>
2020-05-11 22:17:41 +0200

    rowdata.hpp: Minor indentation fixes.

--- pv/data/decode/rowdata.hpp
+++ pv/data/decode/rowdata.hpp
@@ -36,11 +36,11 @@ using std::unordered_map;
 
 #if QT_VERSION < QT_VERSION_CHECK(5, 14, 0)
 namespace std {
-  template<> struct hash<QString> {
-    std::size_t operator()(const QString& s) const noexcept {
-      return (size_t) qHash(s);
-    }
-  };
+	template<> struct hash<QString> {
+		std::size_t operator()(const QString& s) const noexcept {
+			return (size_t) qHash(s);
+		}
+	};
 }
 #endif
 
